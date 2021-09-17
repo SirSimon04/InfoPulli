9fcd6ce48720917115304d88bfc661de3e7b086d
SirSimon04 <simi@engelnetz.de>
2021-09-17 11:02:29 +0200

    added map and getting position

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,9 @@
 import "package:flutter/material.dart";
 import "package:flutter_map/flutter_map.dart";
-import "package:latlong/latlong.dart";
+import 'package:latlong2/latlong.dart';
 import "package:http/http.dart" as http;
 import "dart:convert" as convert;
+import 'package:geolocator/geolocator.dart';
 
 void main() => runApp(MyApp());
 
@@ -16,12 +17,32 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class HomeScreen extends StatelessWidget {
-  final String apiKey = "YOUR_API_KEY";
+class HomeScreen extends StatefulWidget {
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  final String apiKey = "uHuXYU2hIlocJtD1UgIwV0O8omx8sZHv";
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    print("initstate");
+    getPosition();
+  }
+
+  Future<void> getPosition() async {
+    print("here");
+    Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.best);
+    print(position);
+  }
 
   @override
   Widget build(BuildContext context) {
-    final tomtomHQ = new LatLng(52.376372, 4.908066);
+    //final tomtomHQ = latLng.LatLng(52.376372, 4.908066);
     return MaterialApp(
       title: "TomTom Map",
       home: Scaffold(
@@ -29,12 +50,27 @@ class HomeScreen extends StatelessWidget {
             child: Stack(
           children: <Widget>[
             FlutterMap(
-              options: new MapOptions(center: tomtomHQ, zoom: 13.0),
+              options:
+                  MapOptions(center: LatLng(51.388514, 7.000371), zoom: 13.0),
               layers: [
-                new TileLayerOptions(
+                TileLayerOptions(
                   urlTemplate: "https://api.tomtom.com/map/1/tile/basic/main/"
                       "{z}/{x}/{y}.png?key={apiKey}",
                   additionalOptions: {"apiKey": apiKey},
+                ),
+                MarkerLayerOptions(
+                  markers: [
+                    Marker(
+                      width: 80.0,
+                      height: 80.0,
+                      point: LatLng(51.388514, 7.000371),
+                      builder: (BuildContext context) => const Icon(
+                        Icons.location_on,
+                        size: 60.0,
+                        color: Colors.green,
+                      ),
+                    ),
+                  ],
                 )
               ],
             )
