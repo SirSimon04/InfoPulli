b610eb5abead5566bc1f0301e2f626254d7469f0
SirSimon04 <simi@engelnetz.de>
2021-09-30 17:49:36 +0200

    getting leaderboard info from server, other colors for first three

--- lib/screens/leaderboard/leaderboard.dart
+++ lib/screens/leaderboard/leaderboard.dart
@@ -9,81 +9,78 @@ class LeaderBoard extends StatefulWidget {
 }
 
 class _LeaderBoardState extends State<LeaderBoard> {
-  final List<String> names = <String>[
-    "Simon Engel",
-    "Lukas Krinke",
-    "Lukas Baginski",
-    "Simon Engel",
-    "Lukas Krinke",
-    "Lukas Baginski",
-    "Simon Engel",
-    "Lukas Krinke",
-    "Lukas Baginski",
-    "Simon Engel",
-    "Lukas Krinke",
-    "Lukas Baginski",
-  ];
-  final List<int> scanCodes = <int>[
-    100,
-    5,
-    4,
-    100,
-    5,
-    4,
-    100,
-    5,
-    4,
-    100,
-    5,
-    4,
-  ];
-  final List<String> shorts = <String>[
-    "engel",
-    "krinke",
-    "baginski",
-    "engel",
-    "krinke",
-    "baginski",
-    "engel",
-    "krinke",
-    "baginski",
-    "engel",
-    "krinke",
-    "baginski",
-  ];
+  List<String> names = [];
+  List<int> scanCodes = [];
+  List<String> shorts = [];
 
   @override
   void initState() {
     // TODO: implement initState
     super.initState();
-    Network().getCount();
+    getCounts();
+  }
+
+  Future<void> getCounts() async {
+    var scans = await Network().getCount();
+    print("scans " + scans[0]["short"].toString());
+    List<String> n = [];
+    List<String> s = [];
+    List<int> sC = [];
+    for (var person in scans) {
+      n.add(person["first"] + person["last"]);
+      s.add(person["short"]);
+      sC.add(person["count"]);
+    }
+
+    setState(() {
+      names = n;
+      scanCodes = sC;
+      shorts = s;
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.blueGrey[900],
-      body: SingleChildScrollView(
-        child: ListView.separated(
-          shrinkWrap: true,
-          padding: const EdgeInsets.all(8),
-          itemCount: names.length,
-          itemBuilder: (BuildContext context, int index) {
-            return ListTile(
-              contentPadding: const EdgeInsets.all(8.0),
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(15.0),
+      body: names.isEmpty
+          ? const CircularProgressIndicator()
+          : SingleChildScrollView(
+              child: ListView.separated(
+                shrinkWrap: true,
+                padding: const EdgeInsets.all(8),
+                itemCount: names.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return ListTile(
+                    contentPadding: const EdgeInsets.all(8.0),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15.0),
+                    ),
+                    tileColor: getColor(index),
+                    title: Text(names[index]),
+                    leading: Image.asset("images/${shorts[index]}.png"),
+                    trailing: Padding(
+                      padding: const EdgeInsets.only(right: 32),
+                      child: Text(scanCodes[index].toString()),
+                    ),
+                  );
+                },
+                separatorBuilder: (BuildContext context, int index) =>
+                    const Divider(),
               ),
-              tileColor: Colors.blueGrey[100],
-              title: Text(names[index]),
-              leading: Image.asset("images/${shorts[index]}.png"),
-              trailing: Text(scanCodes[index].toString()),
-            );
-          },
-          separatorBuilder: (BuildContext context, int index) =>
-              const Divider(),
-        ),
-      ),
+            ),
     );
+  }
+
+  Color getColor(int index) {
+    if (index == 0) {
+      return const Color(0xffAF9500);
+    } else if (index == 1) {
+      return const Color(0xffD7D7D7);
+    } else if (index == 2) {
+      return const Color(0xff6A3805);
+    } else {
+      return Colors.blueGrey;
+    }
   }
 }
